ea7b19da2f5449aa8862538d2a4facd327624573
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-02 13:24:33 -0700

    1). Completed the route to get all categories in the category-routes.js file.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    const categories = await Category.findAll( {
+      // be sure to include its associated Products
+      include : [ { model : Product, as : "products"} ]
+    } );
+    if( !categories ) return res.status( 404 ).json( { message : "Categories Not Found" } );
+    res.status( 200 ).json( categories );
+  } catch( error ) {
+    res.status( 500 ).json( error );
+  }
 });
 
 router.get('/:id', (req, res) => {
