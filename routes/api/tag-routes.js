ee30a5614f1746762593a2724647a60b79517c19
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-02 20:16:12 -0700

    1). Created delete route to delete a Single Tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,8 +45,19 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy( {
+      where : {
+        id : req.params.id
+      }
+    } );
+    if( !deletedTag ) return res.status( 404 ).json( { message : "Tag Not Found and therefore cannot be deleted..." } );
+    res.status( 200 ).json( deletedTag );
+  } catch( error ) {
+    res.status( 500 ).json( error );
+  }
 });
 
 module.exports = router;
