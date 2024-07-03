4569636682ecb544a437dc2ce2f25dbed4394230
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-03 11:46:52 -0700

    1). Created a put route for updating tags.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update( { tag_name : req.body.tag_name }, {
+      where : {
+        id : req.params.id
+      }
+    } );
+    console.log( updatedTag );
+    if( !updatedTag ) return res.status( 404 ).json( { message : "Tag Not Found and therefore cannot be updated..." } );
+    res.status( 200 ).json( updatedTag );
+  } catch( error ) {
+    res.status( 500 ).json( error );
+  }
 });
 
 router.delete('/:id', async (req, res) => {
