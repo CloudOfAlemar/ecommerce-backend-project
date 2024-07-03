b1d7ca93cdfd5016f4502c5f11d6908a8f239141
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-02 20:04:41 -0700

    1). Created route to get a Single tag and its products.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,9 +17,18 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk( req.params.id, {
+      include : [ { model : Product, through : ProductTag, as : "products" } ]
+    } );
+    if( !tag ) return res.status( 404 ).json( { message : "Tag Not Found" } );
+    res.status( 200 ).json( tag );
+  } catch( error ) {
+    res.status( 500 ).json( error );
+  }
 });
 
 router.post('/', (req, res) => {
