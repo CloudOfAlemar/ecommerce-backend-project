db3ae2966740fd5388799050ce6489857794c8fa
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-02 19:49:14 -0700

    1). Created the route to delete a product.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,8 +117,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy( {
+      where : {
+        id : req.params.id
+      }
+    } );
+    if( !deletedProduct ) return res.status( 404 ).json( { message : "Product Not Found and therefore cannot be deleted..." } );
+    res.status( 200 ).json( deletedProduct );
+  } catch( error ) {
+    res.status( 500 ).json( error );
+  }
 });
 
 module.exports = router;
